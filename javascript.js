bd893d6d1fe094ba90406c4bff256501e8562db1
ovaserashid <159573241+Ovaserashid@users.noreply.github.com>
2024-06-16 17:20:50 +0530

    Change layout, add mouseenter and radio buttons
    Changed the layout to static 512px by 512px. The grids
    change the height and width depending on the number
    of grids the user wants.
    
    Also added the mouseeenter event listener to each grid.
    The grid changes color when mouse enters the grid. The
    colors have two options based on the radio button selected
    by the user. The two buttons correspond to grayscale and
    RGB mode.

--- javascript.js
+++ javascript.js
@@ -1,38 +1,115 @@
 
-const container = document.querySelector('#container');
+const inputBox = document.querySelector('.inputbox');
+const errorMsg = document.createElement('p');
+const button = document.querySelector('.btn');
+const inputField = document.querySelector('.inputvalue');
+const container = document.querySelector('.gridbox');
+const gridContainer = document.createElement('div');
+const radioBlackButton = document.querySelector('.bw');
+const radioRgbButton = document.querySelector('.rgb');
+
 let grid = [];
-function userInputLayout(){
-    const inputBox = document.createElement('div');
-    const inputBoxChildDiv = document.createElement('div');
-    const button = document.createElement('button');
-    const inputField = document.createElement('form');
-    const headingBox = document.createElement('h1');
-    headingBox.textContent = 'Enter the grid value';
-    inputBox.appendChild(headingBox);
-    inputBoxChildDiv.style.display = "flex";
-    button.textContent = 'Go';
-    inputField.appendChild(inputBoxChildDiv);
-    inputField.appendChild(button);
-    inputBox.appendChild(inputField);
+let inputValue;
+let errorSignal = '';
+let counter = 1;
+let randomNumber;
+let brightness = 0;
+
+function designGridContainer(){
+    gridContainer.style.width = "512px";
+    gridContainer.style.height = "512px";
+    gridContainer.style.display = "flex";
+    gridContainer.style.flexWrap = "wrap";
 }
+
+function displayError(){
+    errorMsg.textContent = "Please enter the value between 1 and 100 only";
+    errorMsg.style.color = "red";
+    errorMsg.style.fontStyle = "italic";
+    inputBox.appendChild(errorMsg);
+    errorSignal = 'error';
+}
+
 function getUserInput(){
-    
+    return inputField.value;
 }
-function styleGrid(){
-    grid.style.width = "16px";
-    grid.style.height = "16px";
-    grid.style.border = "2px solid black";
-}
-function appendGrid(){
-    for(let i=0;i<256;i++){
+
+function appendGrid(input){
+    const num = input*input;
+    while(gridContainer.hasChildNodes()){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    if(errorSignal === 'error'){
+        inputBox.removeChild(errorMsg);
+        errorSignal = 'resolved';
+    }
+    for(let i=0;i<num;i++){
         grid[i] = document.createElement('div');
         grid[i].setAttribute('class','grid'+i);
-        grid[i].style.width = "32px";
-        grid[i].style.height = "32px";
+        grid[i].style.width = 512/input+"px";
+        grid[i].style.height = 512/input+"px";
         grid[i].style.border = "1px solid black";
-        container.appendChild(grid[i]);
+        gridContainer.appendChild(grid[i]);
+        grid[i].addEventListener('mouseenter', (e)=>{
+            if(radioRgbButton.checked){
+                randomNumber = Math.floor(Math.random()*3);
+                if(randomNumber === 0){
+                    if(brightness === 1){
+                        counter = 1;
+                        brightness =0;
+                    }
+                    brightness = counter/10;
+                    grid[i].style.backgroundColor = "red";
+                    grid[i].style.opacity = brightness;
+                    counter += 1;
+                }else if(randomNumber === 1){
+                    if(brightness === 1){
+                        brightness = 0;
+                        counter =1;
+                    }
+                    brightness = counter/10;
+                    grid[i].style.backgroundColor = "green";
+                    grid[i].style.opacity = brightness;
+                    counter += 1;
+                }else if(randomNumber === 2){
+                    if(brightness === 1){
+                        brightness = 0;
+                        counter=1;
+                    }
+                    brightness = counter/10;
+                    grid[i].style.backgroundColor = "blue";
+                    grid[i].style.opacity = brightness;
+                    counter += 1;
+                }
+            }else if(radioBlackButton.checked){
+                if(brightness === 1){
+                        counter = 1;
+                        brightness =0;
+                    }else {
+                        brightness = counter/10;
+                        grid[i].style.backgroundColor = "black";
+                        grid[i].style.opacity = brightness;
+                        counter += 1;
+                    }
+                } 
+        });
     }
+    container.appendChild(gridContainer);
 }
-userInputLayout();
-getUserInput();
-appendGrid();
+designGridContainer();
+radioBlackButton.addEventListener('click', ()=>{
+    radioRgbButton.checked = false;
+});
+radioRgbButton.addEventListener('click', ()=>{
+    radioBlackButton.checked = false;
+});
+button.addEventListener('click', ()=> {
+    inputValue = +getUserInput();
+    if(inputValue >= 1 && inputValue <= 100){
+        brightness = 0;
+        counter= 1;
+        appendGrid(inputValue);
+    }else {
+        displayError();
+    }
+});
